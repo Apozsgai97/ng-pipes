4336d2e1cd487b3304c5853fb5e519a607a415b9
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-05 13:48:34 +0200

    Refactor TemperaturePipe to support input and output type parameters for temperature conversion

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,16 +5,36 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: string | number) {
-    let numberValue: number;
+  transform(
+    value: string | number | null,
+    inputType: 'C' | 'F',
+    outputType?: 'C' | 'F'
+  ) {
     
+    if(!value) {
+      return value;
+    }
+    
+    let numberValue: number;
+
     if (typeof value === 'string') {
       numberValue = parseFloat(value);
     } else {
       numberValue = value;
     }
-    const outputTemperature = numberValue * (9 / 5) + 32; // Convert to Fahrenheit
 
-    return `${outputTemperature} °F`;
+    outputType = outputType || inputType;
+
+    if (inputType === outputType) {
+      return `${numberValue.toFixed(2)} °${outputType}`;
+    }
+
+    if (inputType === 'C' && outputType === 'F') {
+      const outputTemperature = numberValue * (9 / 5) + 32; 
+      return `${outputTemperature.toFixed(2)} °F`;
+    } else {
+      const outputTemperature = (numberValue - 32) * (5 / 9); 
+      return `${outputTemperature.toFixed(2)} °C`;
+    }
   }
 }
